7effe418477e54e67578816aa79d9d33fae207b1
David Iskander <davidiskander_work@Davids-MacBook-AliveCor.local>
2016-10-02 11:25:59 -0700

    Add UI for new patient vc

---
+++ NewAppointmentVC.m
@@ -0,0 +1,27 @@
+//
+//  NewAppointmentVC.m
+//  aDoctor
+//
+//  Created by David Iskander on 10/1/16.
+//  Copyright © 2016 David Iskander. All rights reserved.
+//
+
+#import "NewAppointmentVC.h"
+
+@interface NewAppointmentVC ()<UITextFieldDelegate>
+
+@end
+
+@implementation NewAppointmentVC
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+
+
+@end
---
+++ aDoctor/NewPatientVC.h
@@ -0,0 +1,13 @@
+//
+//  NewPatientVC.h
+//  aDoctor
+//
+//  Created by David Iskander on 10/2/16.
+//  Copyright © 2016 David Iskander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewPatientVC : UIViewController
+
+@end
